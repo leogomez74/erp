4ca2556fb271db9c7df2593d0aa4fb11d31d350d
Shift <shift@laravelshift.com>
2023-06-09 00:12:23 +0000

    Adopt class based routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,15 +22,6 @@ class RouteServiceProvider extends ServiceProvider
     public const EMPHOME = 'hrm-dashboard';
 
     /**
-     * The controller namespace for the application.
-     *
-     * When present, controller route declarations will automatically be prefixed with this namespace.
-     *
-     * @var string|null
-     */
-    protected $namespace = 'App\\Http\\Controllers';
-
-    /**
      * Define your route model bindings, pattern filters, etc.
      *
      * @return void
@@ -47,11 +38,9 @@ $this->configureRateLimiting();
         $this->routes(function () {
             Route::prefix('api')
                 ->middleware('api')
-                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });    }
 
